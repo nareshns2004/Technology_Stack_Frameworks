aa53c74908d3746402b4c011998459993c1f3720
nareshns2004 <nareshns2004@gmail.com>
2024-09-08 10:14:00 +0530

    Slight Modification

--- FullStack_Development/Web_Development/Frontend_Web_Development/JavaScript/JavaScript_Basics/JS_Programs.js
+++ FullStack_Development/Web_Development/Frontend_Web_Development/JavaScript/JavaScript_Basics/JS_Programs.js
@@ -1,5 +1,10 @@
 // Write the program to greet a person given their first and last name
 var firstName = "Naresh";
 var lastName = "Kumar";
+var gender = "male";
 
-console.log("Hello Mr. " + firstName + lastName);
+if(gender=="male")
+{
+    console.log("Hello Mr. " + firstName + lastName);
+}
+
